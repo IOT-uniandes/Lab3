cb741aeecfc98fb63e8ce1a2a6aed8a09ef179a5
t-montes <t.montes@uniandes.edu.co>
2023-09-19 16:17:12 -0500

    change name, age, ...

--- RFID_leer_escribir.py
+++ RFID_leer_escribir.py
@@ -1,19 +1,27 @@
 import RFID_RW as rfid
+import json
 
 print("Acerque la tarjeta al lector")
-menu = "Menu Gestión de Puesto de trabajo \n\t1. Asignar Tarjeta a empleado.\n\t2. Leer tarjeta de empleado\n>"
+menu = "Menu Gestión de Puesto de trabajo \n\t1. Asignar Tarjeta a empleado.\n\t2. Leer tarjeta de empleado\n\t3. Salir\n>"
 
 def main():
     while True:
         option = input(menu)
         if option == "1":
-            txt = input("Ingrese el nombre del empleado: \n")
-            rfid.write(txt)
+            info = dict()
+            info["name"] = input("Ingrese el nombre del empleado: \n")
+            info["surname"] = input("Ingrese el apellido del empleado: \n")
+            info["labor"] = input("Ingrese el cargo de trabajo del empleado: \n")
+            info["code"] = input("Ingrese el código del empleado: \n")
+            info["age"] = input("Ingrese la edad del empleado: \n")
+
+            # write json as string
+            rfid.write(json.dumps(info))
             print('Tarjeta Asignada a {}!'.format(rfid.read().rstrip()))
 
         elif option == "2":
-            empleado = rfid.read()
-            msgBienvenida = 'Bienvinid@ {}!'.format(empleado.rstrip())
+            empleado = json.loads(rfid.read())
+            msgBienvenida = f'Bienvinid@ {empleado["name"]} {empleado["surname"]}!, código: {empleado["code"]}, cargo: {empleado["labor"]}, edad: {empleado["age"]}'
             print(msgBienvenida)
 
         elif option == "3":
